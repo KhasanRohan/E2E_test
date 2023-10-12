9a430ca106edf9b5d9762ca0ad9a9a18c287caa1
KhasanRohan <g.nurudinov@mail.ru>
2023-10-12 21:52:58 +0300

    fix3

--- cypress/e2e/project/Test _zadanie.cy.js
+++ cypress/e2e/project/Test _zadanie.cy.js
@@ -18,17 +18,17 @@ describe('Test E2E', () => {
         
         cy.get('input[type="text"]')
             .type('333')
-            .should('have.value', '333')
+            .should('have.value', '333');
         cy.contains('Submit')
             .should('be.visible')
             .click();
-        cy.contains('333').should('exist')    
+        cy.contains('333').should('exist');    
         
         cy.get('input[type="text"]').should('have.value', '');  
         
         cy.get('input[type="text"]')
             .type('abc')
-            .should('have.value', 'abc')
+            .should('have.value', 'abc');
         cy.contains('Submit')
             .should('be.visible')
             .click();
@@ -37,7 +37,7 @@ describe('Test E2E', () => {
 
         cy.get('input[type="text"]')
             .type('@123!')
-            .should('have.value', '@123!')
+            .should('have.value', '@123!');
         cy.contains('Submit')
             .should('be.visible')
             .click();
@@ -56,16 +56,16 @@ describe('Test E2E', () => {
             .uncheck()
             .should('not.have.checked');
         cy.get(':nth-child(1) > span')
-            .should('not.have.css', 'text-decoration', 'line-through solid rgb(89, 89, 89)')
+            .should('not.have.css', 'text-decoration', 'line-through solid rgb(89, 89, 89)');
         
 
         cy.get(':nth-child(1) > button')
             .should('be.visible')
             .click();
         
-        cy.contains('333').should('not.exist')
+        cy.contains('333').should('not.exist');
         
-        cy.get('.TodoItem_item__iFWQW').its('length').should('eq', 2)
+        cy.get('.TodoItem_item__iFWQW').its('length').should('eq', 2);
         
     })
 })
